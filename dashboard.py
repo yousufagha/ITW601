9ff31b7bb2831d3641af09d1859fc00015029c62
sabithashrestha <sabithashrestha@gmail.com>
2025-05-07 18:04:18 +1000

    some updates

--- dashboard.py
+++ dashboard.py
@@ -10,8 +10,7 @@ import plotly.express as px
 
 # Load dataset
 # df = pd.read_csv("enriched_jora_jobs_9000.csv")
-# df = pd.read_csv("cleaned_data.csv")
-df = pd.read_csv("cleaned_data_1.csv")
+df = pd.read_csv("cleaned_data.csv")
 
 # Handle Experience column
 def parse_experience(val):
